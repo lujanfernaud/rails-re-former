9e4909d8e3c8560d28dbe7b5fc18df4fc2cc959e
lujanfernaud <lujanfernaud@gmail.com>
2017-08-15 16:47:28 +0100

    Add route and action to UsersController#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
 
@@ -22,7 +26,7 @@ class UsersController < ApplicationController
     @user.update(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to user_path
     else
       render :edit
     end
