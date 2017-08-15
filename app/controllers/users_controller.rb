771149b2302b0b018406d736e17065bc81784893
lujanfernaud <lujanfernaud@gmail.com>
2017-08-15 17:05:28 +0100

    Redirect UsersController#create to root path

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_user_path
+      redirect_to root_path
     else
       render :new
     end
